362f0a909f7c1e7f776cc11c754c015328763c5b
unknown <andrey.detochkin@gmail.com>
2023-05-14 13:51:43 +0200

    dark mode implementation started

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -4,14 +4,17 @@ import Footer from "../Footer/Footer";
 import Illustartion1 from '../../img/Study Gal Characters1.png'
 import LandingSection2 from "./LandingSection2";
 import LandingSection3 from "./LandingSection3";
+import {ThemeContext, theme, toggleTheme} from "../../App"
 
 
-export default function Landing() {
+export default function Landing(props) {
   return (
     <>
       <div>
       
-        <div className="landing-body">
+        <div className="landing-body" 
+        // id={theme}
+        >
         <div className="landing-text-and-button-container">
         <h1 className="lp-h1">Share your passion and help others to grow</h1>
           <button className="sign-up-for-free-button">SIGN UP FOR FREE</button>
