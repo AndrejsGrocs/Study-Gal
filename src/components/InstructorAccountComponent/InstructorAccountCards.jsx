b74080de4da5bd88a1d488f10f198cc50442f376
unknown <andrey.detochkin@gmail.com>
2023-11-03 16:26:58 +0200

    teachers account personal data designed

--- src/components/InstructorAccountComponent/InstructorAccountCards.jsx
+++ src/components/InstructorAccountComponent/InstructorAccountCards.jsx
@@ -1,50 +1,58 @@
-import React from 'react'
+import React from "react";
 
 function InstructorAccountCards(props) {
-
-
-    const { isntdata } = props;
+  const { isntdata } = props;
 
   return (
     <>
-    
-      <div className="instructor-card">
-      <div className="instructor-card-content">
-    
-        <img
-        className=""
-        src={isntdata.instr_img_src}
-        alt="instructor-account-avatar-image"
-      ></img>
-        <h2 className="instructor-card-title">Instructor name {isntdata.inst_name}</h2>
-        <p className="instructor-card-amount" >{isntdata.skills}</p>
-        <p className="instructor-card-increase" >COURSES {isntdata.amount_of_courses}</p>
-        <p className="instructor-card-increase" >DOWNLOADABLES {isntdata.amount_of_downloadables}</p>
-        <p className="instructor-card-increase" >COACHING SESSIONS BOOKED {isntdata.amount_of_coaching_sessions}</p>
+      <div className="instructor-card1">
+        <div className="instructor-card-content">
+          <img
+            className="instructor-account-avatar"
+            src={isntdata.instr_img_src}
+            alt="instructor-account-avatar-image"
+          ></img>
+          <h2 className="instructor-card-title di">{isntdata.inst_name}</h2>
+          <p className="instructor-card-skills">{isntdata.skills}</p>
 
-        
+          <div className="instructor-personal-account-data-cards">
+            <div className="instructor-account-card">
+              <div className="instructor-card-content">
+                <p className="instructor-card-title d">COURSES </p>
+                <p className="instructor-personal-account-card">
+                  {isntdata.amount_of_courses}
+                </p>
+              </div>
+            </div>
 
-        <div>
-          
+            <div className="instructor-account-card">
+              <div className="instructor-card-content">
+                <p className="instructor-card-title d">DOWNLOADABLES</p>
+                <p className="instructor-personal-account-card">
+                  {isntdata.amount_of_downloadables}
+                </p>
+              </div>
+            </div>
 
+            <div className="instructor-account-card">
+              <div className="instructor-card-content">
+                <p className="instructor-card-title d" >
+                  COACHING SESSIONS BOOKED
+                </p>
+                <p className="instructor-personal-account-card">
+                  {isntdata.amount_of_coaching_sessions}
+                </p>
+              </div>
+            </div>
+          </div>
 
+          <div></div>
 
-
+          <div>{/* <button onClick={() => onAdd(product)}>Add</button> */}</div>
         </div>
-
-
-        
-        
-        
-
-
-        <div>{/* <button onClick={() => onAdd(product)}>Add</button> */}</div>
       </div>
-      </div>
-
-      
     </>
-  )
+  );
 }
 
-export default InstructorAccountCards
+export default InstructorAccountCards;
