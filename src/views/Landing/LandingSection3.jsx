b1e13f8dfbbee2e98e2ed495b192004ab0a536bf
unknown <andrey.detochkin@gmail.com>
2024-01-19 15:33:21 +0200

    landing section 3 app icons responsiveness update

--- src/views/Landing/LandingSection3.jsx
+++ src/views/Landing/LandingSection3.jsx
@@ -17,19 +17,39 @@ function LandingSection3() {
    <div className="cards-image-container">
 <img className="cards-image" src={CardsImage}></img>
 <img className="cards-image-light" src={CardsImageLight}></img>
+<div className='store-icons-container'>
+        <h1 className='store-icons-h1'>Download App</h1>
+
+        <a className='store-button-link' href="https://play.google.com/" target="_blank" rel="noreferrer">
+        <img className="google-play-button-image" src={GooglePlayDark}></img>
+      </a>
+
+
+      <a className='store-button-link' href="https://play.google.com/" target="_blank" rel="noreferrer">
+        <img className="google-play-button-image-light" src={GooglePlayLight}></img>
+      </a>
+
+
+      <a className='store-button-link' href="https://www.apple.com/app-store/" target="_blank" rel="noreferrer">
+      <img className="app-store-button-image" src={AppStoreDark}></img>
+      </a>
+
+      <a className='store-button-link' href="https://www.apple.com/app-store/" target="_blank" rel="noreferrer">
+      <img className="app-store-button-image-light" src={AppStoreLight}></img>
+      </a>
+
+
+
+
+
+
+</div>
 
 
 
 
         </div>
-        <div className='store-icons-container'>
-        <h1 className='store-icons-h1'>Download App</h1>
-<img className="google-play-button-image" src={GooglePlayDark}></img>
-<img className="google-play-button-image-light" src={GooglePlayLight}></img>
-<img className="app-store-button-image" src={AppStoreDark}></img>
-<img className="app-store-button-image-light" src={AppStoreLight}></img>
-
-</div>
+     
         
 
 
