d1e30b6532e865a178f5c325157a3a87565ac1dc
unknown <andrey.detochkin@gmail.com>
2024-01-25 15:53:41 +0200

    login page responsiveness update

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,47 +1,55 @@
-import React from 'react'
-import Illustartion2 from '../../img/Study Gal Characters2.png'
+import React from "react";
+import Illustartion2 from "../../img/Study Gal Characters2.png";
 import { Link } from "react-router-dom";
 import Footer from "../Footer/Footer";
-
 
 export default function LogIn() {
   return (
     <>
-    <div className='login-page-body'>
+      <div className="login-page-body">
+        <form className="login-form">
+          <h1 className="logp-h1">LOGIN WITH STUDY GAL</h1>
 
-<form className='login-form'  >
-<h1 className='logp-h1'>LOGIN WITH STUDY GAL</h1>
-  
-   
-        <input className='form-input' type='email' name='reply_to' placeholder='Email*'   required/>
-        <input className='form-input' type='password' name='reply_to' placeholder='Password*'   required/>
+          <input
+            className="form-input"
+            type="email"
+            name="reply_to"
+            placeholder="Email*"
+            required
+          />
+          <input
+            className="form-input"
+            type="password"
+            name="reply_to"
+            placeholder="Password*"
+            required
+          />
 
-        
-        <button className='btn-login' type='submit' value='Send'>LOGIN</button>
+          <button className="btn-login" type="submit" value="Send">
+            LOGIN
+          </button>
 
-        <p className='form-bottom-text'>New to Study Gal? 
-        <Link
-                    to="/signup-page"
-                    activeclassname="active"
-                    className="nav-link-login"
-                  > Sign Up
-                  </Link>
-                  </p>
+          <p className="form-bottom-text">
+            New to Study Gal?
+            <Link
+              to="/signup-page"
+              activeclassname="active"
+              className="nav-link-login"
+            >
+              {" "}
+              Sign Up
+            </Link>
+          </p>
+       
+        </form>
         <div className="sign-up-page-illustration-container">
-          <img className="login-illustration-1" src={Illustartion2}></img>
-
+            <img className="login-illustration-1" src={Illustartion2}></img>
           </div>
-       
-       
-    
-        
-      </form>
-      
-    </div>
+      </div>
 
-        <div>
-          <Footer />
-        </div>
-        </>
-  )
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
 }
